cefce270736f8b537fea349d08c9c33e2370343e
Amazingct <amazing.comptech@gmail.com>
2020-01-01 02:51:17 +0100

    remove unnecessry imports

--- first_NN.py
+++ first_NN.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-import matplotlib.pyplot as plt
 import cv2
 # we are dealing with images of 28*28 pixels, which will be flatten to 784 input for our NN
 # 128 hidden neurons (1 layer)
